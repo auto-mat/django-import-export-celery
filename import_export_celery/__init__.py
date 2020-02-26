f6dc5b3dfafca43f2854d0e0619a7f7f484a6a1c
Moritz Federspiel <moritz.federspiel@steadysense.at>
2020-02-26 12:39:38 +0100

    Fix celery app import

--- import_export_celery/__init__.py
+++ import_export_celery/__init__.py
@@ -1,1 +1,4 @@
-from project.celery import app as celery_app
+from django.conf import settings
+import importlib
+celery_module = getattr(settings, 'IMPORT_EXPORT_CELERY_INIT_MODULE', 'project.celery')
+celery_app = importlib.import_module(celery_module).app
