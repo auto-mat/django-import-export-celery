7e7295d0ffbed1282c14f4c68794fb20c21a76ab
zdytch <zdytch@gmail.com>
2020-07-28 22:20:43 +0300

    Remove legacy code from __init__.py

--- import_export_celery/__init__.py
+++ import_export_celery/__init__.py
@@ -1,8 +1,1 @@
-import importlib
 
-from django.conf import settings
-
-celery_module = getattr(settings, "IMPORT_EXPORT_CELERY_INIT_MODULE", "project.celery")
-celery_app = importlib.import_module(celery_module).app
-
-__all__ = ("celery_app",)
