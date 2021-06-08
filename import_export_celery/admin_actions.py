c70b7e5037ca355358bc073ce4027613b8e14484
Alaa <alaanourali@gmail.com>
2021-06-08 22:57:47 +0200

    fix linting

--- import_export_celery/admin_actions.py
+++ import_export_celery/admin_actions.py
@@ -45,7 +45,12 @@ def create_export_job_action(modeladmin, request, queryset):
         ej = ExportJob.objects.create(
             app_label=arbitrary_obj._meta.app_label,
             model=arbitrary_obj._meta.model_name,
-            queryset=json.dumps([str(obj.pk) if isinstance(obj.pk, UUID) else obj.pk for obj in queryset]),
+            queryset=json.dumps(
+                [
+                    str(obj.pk) if isinstance(obj.pk, UUID) else obj.pk
+                    for obj in queryset
+                ]
+            ),
             site_of_origin=request.scheme + "://" + request.get_host(),
         )
     rurl = reverse(
