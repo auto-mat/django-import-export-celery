f52326f93eaa5fd91dd40e52380ab24b5736b71f
Jaspreet-singh-1032 <jassimehra2612@gmail.com>
2023-03-26 19:54:05 +0530

    use template to send mail for export job completion

--- example/project/settings.py
+++ example/project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "winners.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
