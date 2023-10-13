d8357e76f750b1c34cd73943e1a976b01dc87df6
Jakub Szafrański <s@samu.pl>
2023-10-13 11:05:07 +0200

    Fix: Lazy-initialize storage class to prevent unwanted migration generation

--- import_export_celery/fields.py
+++ import_export_celery/fields.py
@@ -3,11 +3,15 @@ from django.core.files.storage import get_storage_class
 from django.db import models
 
 
+def lazy_initialize_storage_class():
+    # If the user has specified a custom storage backend, use it.
+    if getattr(settings, "IMPORT_EXPORT_CELERY_STORAGE", None):
+        storage_class = get_storage_class(settings.IMPORT_EXPORT_CELERY_STORAGE)
+        return storage_class()
+    return None
+
+
 class ImportExportFileField(models.FileField):
     def __init__(self, *args, **kwargs):
-        # If the user has specified a custom storage backend, use it.
-        if getattr(settings, "IMPORT_EXPORT_CELERY_STORAGE", None):
-            storage_class = get_storage_class(settings.IMPORT_EXPORT_CELERY_STORAGE)
-            kwargs["storage"] = storage_class()
-
+        kwargs["storage"] = lazy_initialize_storage_class
         super().__init__(*args, **kwargs)
