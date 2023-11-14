90a34d50b73373c36468c4905517e06718296dd2
Petr Dlouhý <petr.dlouhy@email.cz>
2023-11-14 12:29:09 +0100

    add support for Django 4.2 storages

--- import_export_celery/fields.py
+++ import_export_celery/fields.py
@@ -1,13 +1,17 @@
 from django.conf import settings
-from django.core.files.storage import get_storage_class
+from django.core.files.storage import get_storage_class, storages
 from django.db import models
 
 
 def lazy_initialize_storage_class():
     # If the user has specified a custom storage backend, use it.
     if getattr(settings, "IMPORT_EXPORT_CELERY_STORAGE", None):
-        storage_class = get_storage_class(settings.IMPORT_EXPORT_CELERY_STORAGE)
-        return storage_class()
+        try:
+            storage_class = get_storage_class(settings.IMPORT_EXPORT_CELERY_STORAGE)
+            return storage_class()
+        except (ImportError, TypeError):
+            return storages[settings.IMPORT_EXPORT_CELERY_STORAGE]
+
     return None
 
 
